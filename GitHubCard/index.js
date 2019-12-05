7a8046dd03b2d5b7674533110a55d1df94b856ad
arashhaji <56742302+arashhaji@users.noreply.github.com>
2019-12-05 14:58:57 -0500

    Update index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,8 +24,6 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
